3915431120e7d78e7bded40389cc5c58eb29e94b
Claire Deflandre <67142033+ClaireObscure@users.noreply.github.com>
2020-09-03 09:52:21 +0200

    Update script.js

--- Exo 5/script.js
+++ Exo 5/script.js
@@ -12,25 +12,3 @@ function eliminer (poisson) {
 }
 
 
-/*
-function get(id) {
-    return document.getElementById(id)
-}
-
-function kill(elt) {
-    elt.textContent = "Je suis un poisson mort"
-}
-
-kill(get("poisson1"))
-
-
-function recup (numpoisson) {
-    let poisson = document.getElementById("poisson"+numpoisson);
-    return poisson
-}
-
-function eliminer(poisson) {
-    var textdep = poisson.innerHTML;
-    var remp = textdep.replace("vivant", "mort");
-    poisson.innerHTML = remp;
-}*/
